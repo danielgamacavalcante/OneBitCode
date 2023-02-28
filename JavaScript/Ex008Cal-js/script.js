5aaa8e835636ec6fd57c8a56f7a23a2edc758c26
danielgamacavalcante <daniel.gama.cavalcante@gmail.com>
2023-02-28 15:18:01 -0300

    calc

--- JavaScript/Ex008Cal-js/script.js
+++ JavaScript/Ex008Cal-js/script.js
@@ -1,4 +1,4 @@
-const main = document.getElementById("main");
+const main = document.querySelector("main");
 const input = document.querySelector("#input");
 const root = document.querySelector(":root");
 const resultInput = document.querySelector("#result");
@@ -24,6 +24,42 @@ const allowedKeys = [
   "%",
   " ",
 ];
+let value;
+document
+  .querySelector("#copyToClipboard")
+  .addEventListener("click", function (ev) {
+    const button = ev.currentTarget;
+
+    if (button.innerText == "Copy") {
+      button.innerText = "Copied!";
+      button.classList.add("success");
+      navigator.clipboard.writeText(resultInput.value);
+      value = resultInput.value;
+    } else {
+      button.innerText = "Copy";
+      if (resultInput.value == value) {
+        resultInput.value = "";
+      }
+      button.classList.remove("success");
+    }
+  });
+// Mudando Tema
+document.querySelector("#themeSwitcher").addEventListener("click", function () {
+  if (main.dataset.theme === "dark") {
+    root.style.setProperty("--bg-color", "#f1f5f9");
+    root.style.setProperty("--border-color", "#aaa");
+    root.style.setProperty("--font-color", "#212529");
+    root.style.setProperty("--primary-color", "#26834a");
+    main.dataset.theme = "light";
+  } else {
+    root.style.setProperty("--bg-color", "#212529");
+    root.style.setProperty("--border-color", "#666");
+    root.style.setProperty("--font-color", "#f1f5f9");
+    root.style.setProperty("--primary-color", "#4dff91");
+    main.dataset.theme = "dark";
+  }
+});
+// click nos números e operadores
 document.querySelectorAll(".charKey").forEach(function (charKeyBtn) {
   charKeyBtn.addEventListener("click", function () {
     const value = charKeyBtn.dataset.value;
@@ -31,28 +67,51 @@ document.querySelectorAll(".charKey").forEach(function (charKeyBtn) {
     input.focus();
   });
 });
+// função de limpar o campo input
 document.querySelector("#clear").addEventListener("click", function () {
   input.value = " ";
 });
+// ao pressionar o botão, vai ver se está includio na lista este botão e caso esteja vai adicionar a saída do input
 input.addEventListener("keydown", function (ev) {
   ev.preventDefault();
   if (allowedKeys.includes(ev.key)) {
-    input.value += ev.key;
+    // adiciona nos operadores espaços nas laterais
+    if (
+      ev.key == "/" ||
+      ev.key == "-" ||
+      ev.key == "+" ||
+      ev.key == "*" ||
+      ev.key == "%"
+    ) {
+      input.value += " " + ev.key + " ";
+    } else {
+      input.value += ev.key;
+    }
     return;
   }
+  // se o botão pressionado for o botão de apagar vai apagar
   if (ev.key === "Backspace") {
+    // if para analisar o espaço dos operadores para apagar 3 caracteres de uma vez
     if (input.value.lastIndexOf(" ") == input.value.length - 1) {
       input.value = input.value.slice(0, -3);
     } else {
       input.value = input.value.slice(0, -1);
     }
   }
+  //Se pressionar enter calcula
   if (ev.key === "Enter") {
     calculator();
   }
 });
+//se clicar no igual calcula
 document.getElementById("equal").addEventListener("click", calculator);
 function calculator() {
   const result = eval(input.value);
-  input.value = result;
+  if (result == null || result == undefined || result == " ") {
+    resultInput.value = "ERROR";
+    resultInput.classList.add("error");
+  } else {
+    resultInput.classList.remove("error");
+    resultInput.value = result;
+  }
 }
