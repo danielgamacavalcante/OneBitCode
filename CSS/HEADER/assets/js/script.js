ae5f2b9d8ed6df10988d028b66dd14944fa98036
danielgamacavalcante <daniel.gama.cavalcante@gmail.com>
2023-04-14 19:38:25 -0300

    css header

--- CSS/HEADER/assets/js/script.js
+++ CSS/HEADER/assets/js/script.js
@@ -1,3 +1,8 @@
+let imgMoon = "assets/img/lua-notbg.png";
+let imgSoon = "assets/img/sol-notbg.png";
+const main = document.querySelector("main");
+const root = document.querySelector(":root");
+
 function menuShow() {
   let menuMobile = document.querySelector(".mobile-menu");
   if (menuMobile.classList.contains("open")) {
@@ -8,3 +13,24 @@ function menuShow() {
     // document.querySelector('.icon').src = 'assets/img/'
   }
 }
+
+document.querySelector(".themeSwitcher").addEventListener("click", function () {
+  if (main.dataset.theme === "light") {
+    root.style.setProperty("--color-text", "#f9fafc");
+    root.style.setProperty("--color-background-body", "#767680ad");
+    root.style.setProperty("--color-background-header", "#3031313a");
+    root.style.setProperty("--color-backgroud-button", "#0187a7");
+    root.style.setProperty("--color-btn-hover", "#03bceb");
+    root.style.setProperty("--color-container", "#0186a7ab");
+    main.dataset.theme = "dark";
+    document.querySelector("#img-model").src = imgSoon;
+  } else {
+    root.style.setProperty("--color-text", "#0c0b0b");
+    root.style.setProperty("--color-background-body", "#f4f4f75d");
+    root.style.setProperty("--color-background-header", "#3031313a");
+    root.style.setProperty("--color-backgroud-button", "#0187a7");
+    root.style.setProperty("--color-btn-hover", "#03bceb");
+    main.dataset.theme = "light";
+    document.querySelector("#img-model").src = imgMoon;
+  }
+});
