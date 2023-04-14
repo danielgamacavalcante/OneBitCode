52f10e89dfe8f0bac08b72ffeae057043bf446d3
danielgamacavalcante <daniel.gama.cavalcante@gmail.com>
2023-04-14 07:08:45 -0300

    ex 003

--- JavaScript/Ex003r/index.js
+++ JavaScript/Ex003r/index.js
@@ -1,48 +1,104 @@
 let player = {};
-var arrayPlayer = [];
+let arrayPlayer = [];
 let contador = 0;
 
-function addPlayer() {
-  const position = document.querySelector("#position");
-  const name = document.getElementById("name");
-  const number = document.querySelector("#number");
+const position = document.querySelector("#position");
+const namePlayer = document.getElementById("name");
+const number = document.querySelector("#number");
+const btn = document
+  .querySelector("#btnAdd")
+  .addEventListener("click", function (ev) {
+    player.position = position.value;
+    player.name = namePlayer.value;
+    player.number = number.value;
 
-  player.position = position.value;
-  player.name = name.value;
-  player.number = number.value;
-  console.log(
-    "Dados info:" + player.position + " " + player.name + " " + player.number
-  );
-  const confirmation = confirm(
-    "Escalar: " +
-      name.value +
-      "(" +
-      number.value +
-      ")" +
-      " como " +
-      position.value +
-      " ?"
-  );
-  if (confirmation) {
-    arrayPlayer.push(player);
+    console.log(
+      "Dados info:" + player.position + " " + player.name + " " + player.number
+    );
+    const confirmation = confirm(
+      "Escalar: " +
+        namePlayer.value +
+        "(" +
+        number.value +
+        ")" +
+        " como " +
+        position.value +
+        " ?"
+    );
+    if (confirmation) {
+      console.log(`Contador valor antes de adicionar: ${contador}`);
+      arrayPlayer[contador] = player;
 
-    const teamList = document.querySelector("#teamList");
-    const teamLi = document.createElement("li");
-    teamLi.id = "player-" + number.value;
-    const textPlayer = document.createElement("p");
-    const playerFutebol = arrayPlayer[contador];
-    textPlayer.innerText = `${playerFutebol.position}: ${playerFutebol.name} (${playerFutebol.number})`;
+      const teamList = document.querySelector("#teamList");
+      const teamLi = document.createElement("li");
+      teamLi.id = "player-" + number.value;
+      const textPlayer = document.createElement("p");
+      const playerFutebol = arrayPlayer[contador];
 
-    teamLi.appendChild(textPlayer);
-    teamList.appendChild(teamLi);
+      textPlayer.innerText = `${playerFutebol.position}: ${playerFutebol.name} (${playerFutebol.number})`;
 
-    position.value = "";
-    name.value = "";
-    number.value = "";
-    contador++;
-    console.log("contador: " + contador);
-  }
-}
+      teamLi.appendChild(textPlayer);
+      teamList.appendChild(teamLi);
+
+      position.value = "";
+      namePlayer.value = "";
+      number.value = "";
+      contador = contador + 1;
+      console.log("contador incrementado: " + contador);
+      for (let cont = 0; cont <= arrayPlayer.length - 1; cont++) {
+        console.log(cont);
+        console.log(arrayPlayer[cont]);
+      }
+    }
+  });
+
+// function addPlayer() {
+//   const position = document.querySelector("#position");
+//   const name = document.getElementById("name");
+//   const number = document.querySelector("#number");
+
+//   player.position = position.value;
+//   player.name = name.value;
+//   player.number = number.value;
+
+//   console.log(
+//     "Dados info:" + player.position + " " + player.name + " " + player.number
+//   );
+//   const confirmation = confirm(
+//     "Escalar: " +
+//       name.value +
+//       "(" +
+//       number.value +
+//       ")" +
+//       " como " +
+//       position.value +
+//       " ?"
+//   );
+//   if (confirmation) {
+//     console.log(`Contador valor antes de adicionar: ${contador}`);
+//     arrayPlayer[contador] = player;
+
+//     const teamList = document.querySelector("#teamList");
+//     const teamLi = document.createElement("li");
+//     teamLi.id = "player-" + number.value;
+//     const textPlayer = document.createElement("p");
+//     const playerFutebol = arrayPlayer[contador];
+
+//     textPlayer.innerText = `${playerFutebol.position}: ${playerFutebol.name} (${playerFutebol.number})`;
+
+//     teamLi.appendChild(textPlayer);
+//     teamList.appendChild(teamLi);
+
+//     position.value = "";
+//     name.value = "";
+//     number.value = "";
+//     contador++;
+//     console.log("contador incrementado: " + contador);
+//     for (let cont = 0; cont <= arrayPlayer.length - 1; cont++) {
+//       console.log(arrayPlayer[cont]);
+//     }
+//   }
+// }
 
 function removePlayer() {
   const numberRemove = document.querySelector("#numberToRemove").value;
@@ -53,7 +109,10 @@ function removePlayer() {
     );
     if (confirmation) {
       console.log("Entrou no confirme");
+      console.log(arrayPlayer.length);
+
       for (let cont = 0; cont <= arrayPlayer.length - 1; cont++) {
+        console.log(cont);
         console.log(arrayPlayer[cont]);
         const playerFutebol = arrayPlayer[cont];
         // console.log("Pegou nome do jogador: " + playerFutebol.name);
